7b79b4fd753e16dd035d70c5b5e70c49e82812c1
Andres-Betancur-Castañeda <ac20061234@gmail.com>
2025-12-01 10:37:15 -0500

    feat: creación de estructura inicial con rutas /, /todos y /registro

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Todos from "./pages/Todos";
+import Register from "./pages/Register";
+import EditTodo from "./pages/EditTodo";
+
+export default function App() {
+  const [todos, setTodos] = useState([]); // <-- Estado global de TODOs
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/todos"
+          element={<Todos todos={todos} setTodos={setTodos} />}
+        />
+        <Route
+          path="/registro"
+          element={<Register todos={todos} setTodos={setTodos} />}
+        />
+        <Route path="/editar" element={<EditTodo />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
